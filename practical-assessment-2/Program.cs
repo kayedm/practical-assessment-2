ea746238d7e1043d560df276189bc6428344abd6
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2026-01-23 17:36:58 -0500

    feat: set up application entry point and initialize controller and view

---
+++ practical-assessment-2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace practical_assessment_2;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        View view = new View();
+        Controller controller = new Controller(view);
+        controller.Run();
+    }
+}
