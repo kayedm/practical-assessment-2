22a8a8bff71f118c3c00176e5ad5807846f0baa6
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2026-01-23 17:37:57 -0500

    feat: add Controller class to handle application logic
    Add methods to:
    - Get user input from the View
    - Read from a file
    - Write to a file

---
+++ practical-assessment-2/Controller.cs
@@ -0,0 +1,55 @@
+﻿namespace practical_assessment_2;
+
+public class Controller {
+
+    private View view;
+
+    public Controller(View view)
+    {
+        this.view = view;
+    }
+
+    public void Run()
+    {
+        bool running =  true;
+        
+        while (running)
+        {
+            int choice = view.OptionsMenu();
+            
+            switch (choice)
+            {
+                case 1: 
+                    WriteToFile();
+                    break;
+                case 2:
+                    ReadFile();
+                    break;
+                case 3:
+                    running = false;
+                    break;
+                default:
+                    view.ShowMessage("Invalid option");
+                    break;
+            }
+        }
+    }
+
+    public void WriteToFile()
+    {
+        view.ShowMessage("Enter file name:");
+        string fileName = view.UserInput();
+        view.ShowMessage("Enter file text:");
+        string fileText = view.UserInput();
+        File.WriteAllText(fileName, fileText);
+    }
+
+    public void ReadFile()
+    {
+        view.ShowMessage("Enter the name of the text file to read from:");
+        string fileName = view.UserInput();
+        string fileText = File.ReadAllText(fileName);
+        view.ShowMessage(fileText);
+    }
+    
+}
