42c26e01c676f371bf41376432fc4185b0ce3ace
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2026-01-23 17:37:31 -0500

    feat: add View class to handle user interaction
    Add methods to:
    - Display a menu and return the users choice
    - Display a custom message
    - Retrieve user input
    - Display file content

---
+++ practical-assessment-2/View.cs
@@ -0,0 +1,34 @@
+﻿namespace practical_assessment_2;
+
+public class View {
+    
+    public int OptionsMenu()
+    {
+        Console.WriteLine("Pick an option:");
+        Console.WriteLine("1) Write file");
+        Console.WriteLine("2) Read file");
+        Console.WriteLine("3) Exit");
+        
+        return int.Parse(Console.ReadLine());
+        
+    }
+    
+    public void ShowMessage(String message)
+    {
+        Console.WriteLine(message);
+    } 
+    
+    public string UserInput()
+    {
+        string input = Console.ReadLine();
+        return input;
+    }
+    
+    public void DisplayFileContent(String fileContent)
+    {
+        Console.Write("Enter text: ");
+        Console.Write(fileContent);
+    }
+    
+    
+}
