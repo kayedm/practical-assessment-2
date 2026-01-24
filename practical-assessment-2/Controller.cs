96b6228622bd0bbca9dff4de206f418fb30bdfb8
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2026-01-24 18:21:00 -0500

    refactor: update controller logic, add comments and documentation

--- practical-assessment-2/Controller.cs
+++ practical-assessment-2/Controller.cs
@@ -1,21 +1,29 @@
-﻿namespace practical_assessment_2;
+﻿//Author: Kayed Mankal
 
+namespace practical_assessment_2;
+
+// This class is responsible for the logic of the program and runs methods from the view. 
+// [1] “W3schools.com,” W3Schools Online Web Tutorials, https://www.w3schools.com/cs/cs_switch.php
+// [2] “W3schools.com,” W3Schools Online Web Tutorials, https://www.w3schools.com/cs/cs_user_input.php
+// [3] Dotnet-Bot, “Int32.TryParse method (system),” (System) | Microsoft Learn, https://learn.microsoft.com/en-us/dotnet/api/system.int32.tryparse?view=net-10.0
+// [4] “ASP.NET MVC Pattern: .NET,” Microsoft, https://dotnet.microsoft.com/en-us/apps/aspnet/mvc
 public class Controller {
 
-    private View view;
-
+    private View _view;
+    
     public Controller(View view)
     {
-        this.view = view;
+        this._view = view;
     }
 
+    // This method displays a menu from the view, and runs method bases on the users choice
     public void Run()
     {
         bool running =  true;
         
         while (running)
         {
-            int choice = view.OptionsMenu();
+            int choice = _view.OptionsMenu();
             
             switch (choice)
             {
@@ -26,30 +34,45 @@ public class Controller {
                     ReadFile();
                     break;
                 case 3:
+                    _view.ShowMessage("Stopping program");
                     running = false;
                     break;
                 default:
-                    view.ShowMessage("Invalid option");
+                    _view.ShowMessage("Invalid option");
                     break;
             }
         }
     }
 
+    // This method is responsible for writing to a text file
+    // It gets a file name and contents from the user and creates a text file based on the input provided from the user
     public void WriteToFile()
     {
-        view.ShowMessage("Enter file name:");
-        string fileName = view.UserInput();
-        view.ShowMessage("Enter file text:");
-        string fileText = view.UserInput();
+        _view.ShowMessage("Enter file text:");
+        string fileText = _view.UserInput();
+     
+        _view.ShowMessage("Enter file name:");
+        string fileName = _view.UserInput();
+        
         File.WriteAllText(fileName, fileText);
     }
-
+    // This method is responsible for reading a file and displaying its content to the user.
     public void ReadFile()
     {
-        view.ShowMessage("Enter the name of the text file to read from:");
-        string fileName = view.UserInput();
-        string fileText = File.ReadAllText(fileName);
-        view.ShowMessage(fileText);
+        _view.ShowMessage("Enter the name of the text file to read from:");
+        string fileName = _view.UserInput();
+        string extension = Path.GetExtension(fileName);
+        
+        if (extension == ".txt")
+        {
+            _view.ShowMessage("File content:");
+            string fileText = File.ReadAllText(fileName);
+            _view.ShowMessage(fileText);
+        }
+        else
+        {
+            _view.ShowMessage("Invalid file type");
+        }
     }
     
 }
