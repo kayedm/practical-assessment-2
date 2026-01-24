2ea00d61f5f2d74e3dbefcd031f9b18b8aa534a3
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2026-01-24 18:25:19 -0500

    chore: add comments and documentation

--- practical-assessment-2/Program.cs
+++ practical-assessment-2/Program.cs
@@ -1,5 +1,13 @@
-﻿namespace practical_assessment_2;
+﻿//Author: Kayed Mankal
 
+namespace practical_assessment_2;
+
+// This class holds the main entry point for the application
+// The main method creates a view and a controller object, passing the view object into the controller and then runs methods from the controller.
+// [1] “W3schools.com,” W3Schools Online Web Tutorials, https://www.w3schools.com/cs/cs_switch.php
+// [2] “W3schools.com,” W3Schools Online Web Tutorials, https://www.w3schools.com/cs/cs_user_input.php
+// [3] Dotnet-Bot, “Int32.TryParse method (system),” (System) | Microsoft Learn, https://learn.microsoft.com/en-us/dotnet/api/system.int32.tryparse?view=net-10.0
+// [4] “ASP.NET MVC Pattern: .NET,” Microsoft, https://dotnet.microsoft.com/en-us/apps/aspnet/mvc
 public class Program
 {
     public static void Main(string[] args)
